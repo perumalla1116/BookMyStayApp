a7a8d0a7ddf287c7d44c64fd0e216bca1dc6b978
perumalla1116 <krishnavamsiperumalla2@gmail.com>
2026-04-08 09:51:03 +0530

    [Perumalla Krishna Vamsi] Added [UC5-BookingRequest]

--- App/src/BookMyStayApp.java
+++ App/src/BookMyStayApp.java
@@ -1,78 +1,74 @@
 import java.util.*;
 
-class Room {
-    int beds;
-    int size;
-    double price;
-
-    Room(int beds, int size, double price) {
-        this.beds = beds;
-        this.size = size;
-        this.price = price;
-    }
-}
-
-class RoomInventory {
-    private Map<String, Integer> availability = new HashMap<>();
-
-    RoomInventory() {
-        availability.put("Single", 5);
-        availability.put("Double", 3);
-        availability.put("Suite", 2);
-    }
-
-    public Map<String, Integer> getRoomAvailability() {
-        return availability;
-    }
-}
-
-class RoomSearchService {
-    public void searchAvailableRooms(
-            RoomInventory inventory,
-            Room singleRoom,
-            Room doubleRoom,
-            Room suiteRoom) {
-
-        Map<String, Integer> availability = inventory.getRoomAvailability();
-
-        System.out.println("Room Search\n");
-
-        if (availability.get("Single") > 0) {
-            System.out.println("Single Room:");
-            System.out.println("Beds: " + singleRoom.beds);
-            System.out.println("Size: " + singleRoom.size + " sqft");
-            System.out.println("Price per night: " + singleRoom.price);
-            System.out.println("Available: " + availability.get("Single") + "\n");
-        }
-
-        if (availability.get("Double") > 0) {
-            System.out.println("Double Room:");
-            System.out.println("Beds: " + doubleRoom.beds);
-            System.out.println("Size: " + doubleRoom.size + " sqft");
-            System.out.println("Price per night: " + doubleRoom.price);
-            System.out.println("Available: " + availability.get("Double") + "\n");
-        }
-
-        if (availability.get("Suite") > 0) {
-            System.out.println("Suite Room:");
-            System.out.println("Beds: " + suiteRoom.beds);
-            System.out.println("Size: " + suiteRoom.size + " sqft");
-            System.out.println("Price per night: " + suiteRoom.price);
-            System.out.println("Available: " + availability.get("Suite"));
-        }
-    }
-}
-
 public class BookMyStayApp {
+
+    static int linearSearchFirst(String[] arr, String target) {
+        int comparisons = 0;
+        for (int i = 0; i < arr.length; i++) {
+            comparisons++;
+            if (arr[i].equals(target)) {
+                System.out.println("Linear First Index: " + i);
+                System.out.println("Comparisons: " + comparisons);
+                return i;
+            }
+        }
+        System.out.println("Not Found");
+        return -1;
+    }
+
+    static int linearSearchLast(String[] arr, String target) {
+        int comparisons = 0;
+        int index = -1;
+        for (int i = 0; i < arr.length; i++) {
+            comparisons++;
+            if (arr[i].equals(target)) {
+                index = i;
+            }
+        }
+        System.out.println("Linear Last Index: " + index);
+        System.out.println("Comparisons: " + comparisons);
+        return index;
+    }
+
+    static int binarySearch(String[] arr, String target) {
+        int l = 0, r = arr.length - 1, comparisons = 0;
+        while (l <= r) {
+            comparisons++;
+            int m = (l + r) / 2;
+            if (arr[m].equals(target)) {
+                System.out.println("Binary Index: " + m);
+                System.out.println("Comparisons: " + comparisons);
+                return m;
+            } else if (arr[m].compareTo(target) < 0) {
+                l = m + 1;
+            } else {
+                r = m - 1;
+            }
+        }
+        System.out.println("Not Found");
+        return -1;
+    }
+
+    static int countOccurrences(String[] arr, String target) {
+        int count = 0;
+        for (String s : arr) {
+            if (s.equals(target)) count++;
+        }
+        return count;
+    }
+
     public static void main(String[] args) {
 
-        Room single = new Room(1, 250, 1500.0);
-        Room doubleRoom = new Room(2, 400, 2500.0);
-        Room suite = new Room(3, 750, 5000.0);
+        String[] logs = {"accB","accA","accB","accC"};
 
-        RoomInventory inventory = new RoomInventory();
+        linearSearchFirst(logs, "accB");
+        linearSearchLast(logs, "accB");
 
-        RoomSearchService service = new RoomSearchService();
-        service.searchAvailableRooms(inventory, single, doubleRoom, suite);
+        Arrays.sort(logs);
+
+        binarySearch(logs, "accB");
+
+        int count = countOccurrences(logs, "accB");
+        System.out.println("Count: " + count);
     }
 }
